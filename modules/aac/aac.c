1f489608f4636b54eec76d383486f516d4a2f322
Andrea Milanta <AndreaMilanta@users.noreply.github.com>
2017-07-04 23:30:03 +0200

    finished all states

--- modules/aac/aac.c
+++ modules/aac/aac.c
@@ -1,51 +1,70 @@
 #include "aac.h"
 
 aac_states aac_currentState;
-int aac_timings[AAC_NUM_ENTRIES];
+int aac_parameters[AAC_NUM_PARAMS];
+int aac_externValues[AAC_NUM_VALUES];
 int aac_dtRelease;      //counter for clutch "slow" release
 float aac_clutchStep;   //step for each "frame" of aac
 
 void aac_execute(void){
     switch (aac_currentState) {
+        case START:
+            Efi_setRPMLimiter();
+            aac_notifySW(READY);
+            aac_currentState = READY;
+            return;
         case READY:
             Clutch_set(100);
             return;
         case START_RELEASE:
-            Clutch_set(aac_timings[RAMP_START]);
-            aac_currentState = RELEASING;
-            aac_dtRelease = aac_timings[RAMP_TIME] / 10;
-            aac_clutchStep = (float)((RAMP_START - RAMP_END) * AAC_WORK_RATE_ms / aac_timings[RAMP_TIME]);
+            Clutch_set(aac_parameters[RAMP_START]);
+            aac_dtRelease = aac_parameters[RAMP_TIME] / 10;
+            aac_clutchStep = (float)((RAMP_START - RAMP_END) * AAC_WORK_RATE_ms / aac_parameters[RAMP_TIME]);
             return;
         case RELEASING:
-//             Clutch_set(aac_timings[RAMP_END] + (aac_clutchStep * aac_dtRelease));        //Works iff the cluth paddle is disabled
+//             Clutch_set(aac_parameters[RAMP_END] + (aac_clutchStep * aac_dtRelease));        //Works iff the cluth paddle is disabled
             Clutch_set(Clutch_get() - aac_clutchStep);
             aac_dtRelease--;
-            if(aac_dtRelease <= 0 || Clutch_get() <= aac_timings[RAMP_END])
+            if(aac_dtRelease <= 0 || Clutch_get() <= aac_parameters[RAMP_END])
                 aac_currentState = RUNNING;
                 Clutch_set(0);
+                Efi_unsetRPMLimiter();
             return;
         case RUNNING:
-        //TODO Check condizioni e cambio 
+        //TODO Check condizioni e cambio
+            if(gearShift_currentGear == 4){
+                aac_currentState = OFF;
+                return;
+            }
+          //parameters for gear is taken using as a index the baseline (RPM_LIMIT_1_2) + (gear - 1)
+            if(aac_externValues[RPM] >= aac_parameters[RPM_LIMIT_1_2 + gearShift_currentGear - 1]
+              && aac_externValues[WHEEL_SPEED] >= aac_parameters[SPEED_LIMIT_1_2 + gearShift_currentGear - 1]){
+                GearShift_up();
+            }
         default: return;
     }
 }
 
-void aac_loadDefaultTimings(void){
+void aac_loadDefaultParams(void){
 //Use defaults only if eeprom unavailable
 #ifndef AAC_EEPROM_H
-    aac_timings[RAMP_START]      = DEF_RAMP_START;
-    aac_timings[RAMP_END]        = DEF_RAMP_END;
-    aac_timings[RAMP_TIME]       = DEF_RAMP_TIME;
-    aac_timings[RPM_LIMIT_1_2]   = DEF_RPM_LIMIT_1_2;
-    aac_timings[RPM_LIMIT_2_3]   = DEF_RPM_LIMIT_2_3;
-    aac_timings[RPM_LIMIT_3_4]   = DEF_RPM_LIMIT_3_4;
-    aac_timings[SPEED_LIMIT_1_2] = DEF_SPEED_LIMIT_1_2;
-    aac_timings[SPEED_LIMIT_2_3] = DEF_SPEED_LIMIT_2_3;
-    aac_timings[SPEED_LIMIT_3_4] = DEF_SPEED_LIMIT_3_4;
+    aac_parameters[RAMP_START]      = DEF_RAMP_START;
+    aac_parameters[RAMP_END]        = DEF_RAMP_END;
+    aac_parameters[RAMP_TIME]       = DEF_RAMP_TIME;
+    aac_parameters[RPM_LIMIT_1_2]   = DEF_RPM_LIMIT_1_2;
+    aac_parameters[RPM_LIMIT_2_3]   = DEF_RPM_LIMIT_2_3;
+    aac_parameters[RPM_LIMIT_3_4]   = DEF_RPM_LIMIT_3_4;
+    aac_parameters[SPEED_LIMIT_1_2] = DEF_SPEED_LIMIT_1_2;
+    aac_parameters[SPEED_LIMIT_2_3] = DEF_SPEED_LIMIT_2_3;
+    aac_parameters[SPEED_LIMIT_3_4] = DEF_SPEED_LIMIT_3_4;
 #endif
 }
 
-void aac_updateTiming(const aac_times id, const int value){
-    if(id < AAC_NUM_ENTRIES)
-        aac_timings[id] = value;
+void aac_updateParam(const aac_params id, const int value){
+    if(id < AAC_NUM_PARAMS)
+        aac_parameters[id] = value;
+}
+
+void aac_notifySW(const aac_notifications mex){
+    //TODO CAN mex to steering wheel
 }
--- modules/aac/aac.h
+++ modules/aac/aac.h
@@ -7,31 +7,49 @@
 
 #include "aac_defaults.h"
 #include "clutch.h"
+#include "gearshift.h"
+#include "efi.h"
 
 #define AAC_WORK_RATE_ms   10 //ms
 #define CLUTCH_PULL_MAX_TIME_s 10
-#define AAC_NUM_ENTRIES     9            //Number of aac_times enum elements
+#define AAC_NUM_PARAMS    9            //Number of aac_params enum elements
+#define AAC_NUM_VALUES    3            //Number of aac_values enum elements
 
 typedef enum{
     OFF,
+    START,
     READY,
     START_RELEASE,
     RELEASING,
     RUNNING
 }aac_states;
 
+//when modifying entries update AAC_NUM_PARAMS
 typedef enum{
     RAMP_START,
     RAMP_END,
     RAMP_TIME,
-    //MANTAIN THE FOLLOWING ORDER!!!!!!!!!
+    //MANTAIN THE ORDER!!!!!!!!!
     RPM_LIMIT_1_2,
-    SPEED_LIMIT_1_2,
     RPM_LIMIT_2_3,
-    SPEED_LIMIT_2_3,
     RPM_LIMIT_3_4,
+    SPEED_LIMIT_1_2,
+    SPEED_LIMIT_2_3,
     SPEED_LIMIT_3_4
-}aac_times;
+}aac_params;
+
+typedef enum{
+    READY,
+    GO,
+    OFF
+}aac_notifications;
+
+//when modifying entries update AAC_NUM_VALUES
+typedef enum{
+    RPM,
+    WHEEL_SPEED,
+    APPS
+}aac_values;
 
 extern unsigned int gearShift_currentGear;
 
@@ -42,10 +60,12 @@ void aac_start(void);
 
 void aac_stop(void);
 
-void aac_loadDefaultTimings(void);
+void aac_loadDefaultParams(void);
 
-void aac_updateTiming(const aac_times id, const int value);
+void aac_updateParam(const aac_params id, const int value);
 
-void aac_forceState(aac_states newState);
+void aac_forceState(const aac_states newState);
+
+void aac_notifySW(const aac_notifications mex);
 
 #endif
