39e06b8dd1a5367672a1e96e3bacf043a277163e
Andrea Milanta <AndreaMilanta@users.noreply.github.com>
2017-07-08 16:35:40 +0200

    updated enum order

--- modules/gcu_rio.h
+++ modules/gcu_rio.h
@@ -39,29 +39,32 @@
 #define RIO_NUM_TIMES 22    //Number of times variables (enums)
 
 typedef enum {
-     //Up
-     DELAY,
-     UP_REBOUND,
-     UP_BRAKE,
+     //Neutral 1->N
+     NT_PUSH_1_N,
+     NT_CLUTCH_1_N,
+     NT_REBOUND_1_N,
+     NT_BRAKE_1_N,
+    //Neutral 2->N
+     NT_PUSH_2_N,
+     NT_CLUTCH_2_N,
+     NT_REBOUND_2_N,
+     NT_BRAKE_2_N,
+    //Downshift
+     DN_PUSH,
+     CLUTCH,
+     DN_REBOUND,
+     DN_BRAKE,
+    //Upshift gear specific
      UP_PUSH_1_2,
      UP_PUSH_2_3,
      UP_PUSH_3_4,
      UP_PUSH_4_5,
-     //down
-     CLUTCH,
-     DN_PUSH,
-     DN_BRAKE,
-     DN_REBOUND,
-     //neutral
+    //Up generics
+     DELAY,
+     UP_REBOUND,
+     UP_BRAKE,
+    //neutral
      NT_CLUTCH_DELAY,
-     NT_REBOUND_1_N,
-     NT_REBOUND_2_N,
-     NT_BRAKE_1_N,
-     NT_BRAKE_2_N,
-     NT_PUSH_1_N,
-     NT_PUSH_2_N,
-     NT_CLUTCH_1_N,
-     NT_CLUTCH_2_N,
 
      //Multiple tries
      DOWN_TIME_CHECK,
